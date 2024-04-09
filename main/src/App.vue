ba5e37fcd0bdd8da492896de3beda7eeab045291
vadym-loik <loikfit@gmail.com>
2024-04-09 18:41:27 +0200

    create project

---
+++ main/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+
+    <div class="wrapper">
+
+
+      <nav>
+        <RouterLink to="/"></RouterLink>
+        <RouterLink to="/"></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
