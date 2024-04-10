09ee3ab530739dfa31cd3fea54d9d8806bd7d8f9
vadym-loik <loikfit@gmail.com>
2024-04-10 23:07:15 +0200

    edit nav section, add notebook icon, new styles, edit rout links

--- main/src/App.vue
+++ main/src/App.vue
@@ -2,36 +2,48 @@
   <div class="container">
 
     <header class="header">
-      <h1 class="title">{{ title }}</h1>
-      <nav class="nav" id="nav">
-        <RouterLink class="nav-link" :to="{ name: 'NoteList' }">All Notes</RouterLink>
-        <RouterLink class="nav-link" :to="{ name: 'AddNote' }">Add New Note</RouterLink>
-      </nav>
+      <div class="header-nav">
+        <div class="header-wrap">
+          <RouterLink class="header-wrap__link" :to="{ name: 'NoteList' }">
+            <h1 class="title">{{ title }}</h1>
+          </RouterLink>
+          <LucideNotebookPen />
+        </div>
+
+        <nav class="nav" id="nav">
+          <RouterLink class="nav-link" :to="{ name: 'NoteList' }">All Notes</RouterLink>
+          <RouterLink class="nav-link" :to="{ name: 'AddNote' }">Add New Note</RouterLink>
+        </nav>
+
+      </div>
     </header>
 
     <main>
       <RouterView />
     </main>
 
-    <!-- <footer>
-      <p class="footer">Copyright 2024</p>
+    <!-- <footer class="footer" id="footer">
+      <p>Copyright 2024</p>
     </footer> -->
   </div>
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import LucideNotebookPen from './components/LucideNotebookPen.vue';
 
 
 export default {
   name: 'App',
   components: {
     RouterLink,
-    RouterView
+    RouterView,
+    LucideNotebookPen,
+
   },
   data() {
     return {
-      title: 'Notes List',
+      title: 'Notes List App',
     }
   }
 }
@@ -39,18 +51,35 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  max-width: 1280px;
+  max-width: 1100px;
   margin: 0 auto;
 }
 
 .header {
-  text-align: center;
   margin: 2rem 0;
+
+  &-wrap {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+
+  }
+
+  &-wrap__link {
+    text-decoration: none;
+    color: #000;
+  }
+
+  &-nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
 
 .title {
   text-align: center;
-  margin-bottom: 2rem;
+
 }
 
 .main {
@@ -61,33 +90,26 @@ export default {
   display: flex;
   gap: 1rem;
   align-items: center;
-  justify-content: center;
+
 
   &-link {
     text-decoration: none;
     color: #000;
+    border: #777 1px solid;
+    padding: 0.5rem 1rem;
+    border-radius: 20px;
     cursor: pointer;
   }
 
   &-link:hover {
-    text-decoration: underline;
-    color: #777;
+    color: aliceblue;
+    background-color: #42b983;
+    transition: all 0.3s ease-in-out;
   }
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-
-.footer {
-  text-align: center;
-  margin-top: 2rem;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  padding: 1rem;
-  background-color: #ccc;
+  background-color: #42b983;
+  color: #fff;
 }
 </style>
