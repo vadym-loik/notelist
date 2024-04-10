1102c6182de689a8e2c69f141c8df572276c03bb
vadym-loik <loikfit@gmail.com>
2024-04-10 23:11:56 +0200

    create notebook icon component

---
+++ main/src/components/LucideNotebookPen.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4M2 6h4m-4 4h4m-4 4h4m-4 4h4"></path>
+            <path d="M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1l1-4Z"></path>
+        </g>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'LucideNotebookPen'
+}
+</script>
