14163eb9d35755c77c7a089661bf0938f6acddd4
vadym-loik <loikfit@gmail.com>
2024-04-10 02:47:09 +0200

    added store import

--- main/src/main.js
+++ main/src/main.js
@@ -3,9 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(router).use(store).mount('#app')
